7340f6d1433b5f48cf05fb2cf0fcbac8e084225f
balazs <balazs@inqmobile.com>
2012-02-11 19:33:23 +0000

    Oyster ID reading POC
    - reads oyster cards
    - displays oyster ID on screen

--- src/com/paranoid/oysterid/android/ReadMifareId.java
+++ src/com/paranoid/oysterid/android/ReadMifareId.java
@@ -1,13 +1,80 @@
+
 package com.paranoid.oysterid.android;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ReadMifareId extends Activity {
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+    private NfcAdapter mAdapter;
+    private PendingIntent mPendingIntent;
+    private TextView mIdText;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+        mIdText = (TextView)findViewById(R.id.oyster_id_text);
+
+        initializeNfc();
+    }
+
+    private void initializeNfc() {
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        mPendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+
+        try {
+            ndef.addDataType("*/*");
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        mFilters = new IntentFilter[] {
+                ndef,
+        };
+
+        // Setup a tech list for all NfcF tags
+        mTechLists = new String[][] {
+            new String[] {
+                    IsoDep.class.getName(), NfcA.class.getName()
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        StringBuffer id = new StringBuffer();
+        for (byte digit : tagFromIntent.getId()) {
+            id.append(String.format("%xd", digit));
+        }
+        mIdText.setText(id);
     }
 }
