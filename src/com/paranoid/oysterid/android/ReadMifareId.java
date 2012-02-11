67e20b01ba305b9e747d6d95743359509befad6d
balazs <balazs@inqmobile.com>
2012-02-11 15:06:14 +0000

    Initial commit
    Initializing project

---
+++ src/com/paranoid/oysterid/android/ReadMifareId.java
@@ -0,0 +1,13 @@
+package com.paranoid.oysterid.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadMifareId extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
